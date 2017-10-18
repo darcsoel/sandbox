788cf86977c4bda7b51110f6805770d09b891e6b
Ross <rostikvunarchuk@gmail.com>
2017-10-18 23:12:04 +0300

    Array operations in separated file

---
+++ array.py
@@ -0,0 +1,128 @@
+import numpy as np
+
+userInput = []
+while True:
+    var = input('Enter a word\n')
+    if len(var) == 0:
+        break
+    else:
+        userInput.append(var)
+
+print('Yoy entered - {0}'.format(userInput))
+
+print('\n')
+
+long_array = np.array(range(1, 100, 2))
+
+print(long_array)
+
+print('\n')
+
+thirdArray = []
+if thirdArray:
+    print('Not empty array')
+else:
+    print('Empty array')
+
+thirdArray.append('cat')
+thirdArray.append('dog')
+thirdArray.append('parrot')
+thirdArray.append('dog')
+thirdArray.append('dog')
+
+print(thirdArray)
+
+while 'dog' in thirdArray:
+    thirdArray.remove('dog')
+
+print(thirdArray)
+print('\n\n')
+
+firstArrayForMerge = ['some', 'val']
+secondArrayForMerge = ['to', 'merge']
+
+print(firstArrayForMerge + secondArrayForMerge)
+
+print("\n\n")
+
+firstDictionary = {}
+print(firstDictionary)
+firstDictionary['key1'] = 'val1'
+firstDictionary['key2'] = 'val2'
+firstDictionary['key3'] = 'val3'
+print(firstDictionary)
+
+# .items() .values() .keys() methods
+for key, value in firstDictionary.items():
+    print('Key - {0} : Value - {1}'.format(key, value))
+
+list_1 = [1, 2, 3, 4]
+list_2 = [1, 2, 3, 4]
+list_3 = [1, 2, 3, 4]
+list_4 = [1, 2, 3, 4]
+
+for _, item in enumerate(list_1):
+    del item
+
+for _, item in enumerate(list_2):
+    list_2.remove(item)
+
+for _, item in enumerate(list_3[:]):
+    list_3.remove(item)
+
+for idx, item in enumerate(list_4):
+    list_4.pop(idx)
+
+print(list_1)
+print(list_2)
+print(list_3)
+print(list_4)
+
+firstArray = ['first', 'second', 'third']
+print(firstArray)
+
+firstArray.append('fourth')
+print(firstArray)
+
+firstArray.insert(0, 'fifth')
+print(firstArray)
+
+firstArray.sort()
+print(firstArray)
+
+print("\n")
+
+firstArray.reverse()
+print(firstArray)
+
+secondArray = firstArray[:]
+
+print('Array length = {0}'.format(len(firstArray)))
+
+firstArray.remove('first')
+print(firstArray)
+
+del firstArray[0]
+print(firstArray)
+
+firstArray.pop()
+print(firstArray)
+
+print("\n\n")
+
+for elem in secondArray:
+    print('Elem - {0}'.format(elem.title()))
+    print('Elem - {0}'.format(elem.upper()))
+    print('Elem - {0}'.format(elem.lower()))
+
+print("\n")
+
+someVal = 'ten'
+if someVal not in secondArray:
+    print('Value \'{0}\' is not in array {1}'.format(someVal, secondArray))
+
+firstStr = 'first'
+if firstStr in secondArray:
+    print('Value \'{0}\' is in {1}'.format(firstStr, secondArray))
+
+print("\n")
--- main.py
+++ main.py
@@ -1,69 +1,8 @@
-from array import array
 from factory.CarFactory import CarFactory
-import numpy as np
-
+import random
 
 # system beep, don`t work on MAC
 print("\a")
-
-# userInput = []
-# while True:
-#     var = input('Enter a word\n')
-#     if len(var) == 0:
-#         break
-#     else:
-#         userInput.append(var)
-#
-# print('Yoy entered - {0}'.format(userInput))
-
-firstArray = ['first', 'second', 'third']
-print(firstArray)
-
-firstArray.append('fourth')
-print(firstArray)
-
-firstArray.insert(0, 'fifth')
-print(firstArray)
-
-firstArray.sort()
-print(firstArray)
-
-print("\n")
-
-firstArray.reverse()
-print(firstArray)
-
-secondArray = firstArray[:]
-
-print('Array length = {0}'.format(len(firstArray)))
-
-firstArray.remove('first')
-print(firstArray)
-
-del firstArray[0]
-print(firstArray)
-
-firstArray.pop()
-print(firstArray)
-
-print("\n\n")
-
-for elem in secondArray:
-    print('Elem - {0}'.format(elem.title()))
-    print('Elem - {0}'.format(elem.upper()))
-    print('Elem - {0}'.format(elem.lower()))
-
-print("\n")
-
-someVal = 'ten'
-if someVal not in secondArray:
-    print('Value \'{0}\' is not in array {1}'.format(someVal, secondArray))
-
-firstStr = 'first'
-if firstStr in secondArray:
-    print('Value \'{0}\' is in {1}'.format(firstStr, secondArray))
-
-print("\n")
 
 for _ in range(1, 5):
     print("Hello")
@@ -73,55 +12,12 @@ print("\n")
 for index in range(1, 5):
     print('Hello {0} : {1}'.format(index, index ** 2))
 
-thirdArray = []
-if thirdArray:
-    print('Not empty array')
-else:
-    print('Empty array')
-
-thirdArray.append('cat')
-thirdArray.append('dog')
-thirdArray.append('parrot')
-thirdArray.append('dog')
-thirdArray.append('dog')
-
-print(thirdArray)
-
-while 'dog' in thirdArray:
-    thirdArray.remove('dog')
-
-print(thirdArray)
-print('\n\n')
-
-firstArrayForMerge = ['some', 'val']
-secondArrayForMerge = ['to', 'merge']
-
-print(firstArrayForMerge + secondArrayForMerge)
-
-print("\n\n")
-
-firstDictionary = {}
-print(firstDictionary)
-firstDictionary['key1'] = 'val1'
-firstDictionary['key2'] = 'val2'
-firstDictionary['key3'] = 'val3'
-print(firstDictionary)
-
-# .items() .values() .keys() methods
-for key, value in firstDictionary.items():
-    print('Key - {0} : Value - {1}'.format(key, value))
-
 a = 32
 b = 7
 print('Modular = {0}'.format(a % b))
 print('OR = {0}'.format(a | b))
 print('AND = {0}'.format(a & b))
 print('XOR = {0}'.format(a ^ b))
-
-strictArray = array('i', [1, 2, 3, 4])
-print(strictArray)
-strictArray.append(1)
-print(strictArray)
 
 
 def fibonacci(number):
@@ -166,28 +62,6 @@ print('Mersenne numbers = {0}'.format(mersenne_numbers(mersenneNumber)))
 car = 'Audi'
 print(CarFactory.create(car))
 
-list_1 = [1, 2, 3, 4]
-list_2 = [1, 2, 3, 4]
-list_3 = [1, 2, 3, 4]
-list_4 = [1, 2, 3, 4]
-
-for _, item in enumerate(list_1):
-    del item
-
-for _, item in enumerate(list_2):
-    list_2.remove(item)
-
-for _, item in enumerate(list_3[:]):
-    list_3.remove(item)
-
-for idx, item in enumerate(list_4):
-    list_4.pop(idx)
-
-print(list_1)
-print(list_2)
-print(list_3)
-print(list_4)
-
 
 def to_upper_case(fn):
     def wrapped():
@@ -209,7 +83,15 @@ print('Number to string -> {0}'.format(str(numberToConvert)))
 print('Octal system -> {0}'.format(oct(numberToConvert)))
 print('Hex octal system -> {0}'.format(hex(numberToConvert)))
 
-long_array = np.array(range(1, 100, 2))
+someWord = 'someword'
+firstVersion = someWord[:]
+anagram = ''
 
-print(long_array)
+while someWord:
+    pos = random.randrange(len(someWord))
+    anagram += someWord[pos]
+    someWord = someWord[:pos] + someWord[pos + 1:]
 
+del someWord
+
+print('{0} -> {1}'.format(firstVersion, anagram))
