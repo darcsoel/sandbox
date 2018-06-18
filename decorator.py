6a160150be4d8010b4ce796e809e0df9c97be504
Ross <rostikvunarchuk@gmail.com>
2018-06-18 23:16:33 +0300

    Cosmetic

--- decorator.py
+++ decorator.py
@@ -2,23 +2,19 @@ import logging
 
 
 def log(func):
-    """
-    Логируем какая функция вызывается
-    """
 
     def wrap_log(*args, **kwargs):
         name = func.__name__
         logger = logging.getLogger(name)
         logger.setLevel(logging.INFO)
 
-        # Открываем файл логов для записи
         fh = logging.FileHandler("%s.log" % name)
         fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         formatter = logging.Formatter(fmt)
         fh.setFormatter(formatter)
         logger.addHandler(fh)
 
-        logger.info("Вызов функции: %s" % name)
+        logger.info("Call function: %s" % name)
         result = func(*args, **kwargs)
         logger.info("Результат: %s" % result)
         return func
@@ -28,9 +24,6 @@ def log(func):
 
 @log
 def double_function(a):
-    """
-    Умножаем полученный параметр
-    """
     return a * 2
 
 
