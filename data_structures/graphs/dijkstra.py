149b385a760e0c2cf333ffcaffb309dbcc878a24
Ross <rostikvunarchuk@gmail.com>
2018-09-16 21:33:01 +0300

    Not finished

--- data_structures/graphs/dijkstra.py
+++ data_structures/graphs/dijkstra.py
@@ -1,12 +1,54 @@
+import sys
+
+
 class Graph:
-    def __init__(self, matrix):
+    def __init__(self, matrix, start_from=0):
         self.matrix = matrix
+        self.start_from = start_from
         self.visited = [False] * len(self.matrix)
-        self.stable = [False] * len(self.matrix)
+        self.way = list()
+        self.wrong_vehicle = None
 
     def calculate(self):
-        for vehicle, connections in enumerate(self.matrix):
-            self.visited[vehicle] = True
+        vehicles = {i: True for i in range(len(self.matrix))}
+        self.visited[self.start_from] = True
+        self.way.insert(0, self.start_from)
+
+        while len(vehicles) > 0:
+            connections = self.matrix[self.start_from]
+
+            if all(connection == 0 for connection in connections) and len(self.way) > 0:
+                self.wrong_vehicle = self.start_from
+                vehicles[self.start_from] = True
+                self.start_from = self.way.pop()
+                continue
+
+            min_weight = sys.maxsize
+            neighbourhoods = [[closest, weight] for closest, weight in enumerate(connections)
+                              if closest != self.start_from and not self.visited[closest]
+                              and closest != self.wrong_vehicle]
+
+            if not neighbourhoods:
+                break
+
+            closest = False
+
+            for closest_vehicle, weight in neighbourhoods:
+                if 0 < weight < min_weight:
+                    min_weight = weight
+                    closest = closest_vehicle
+
+            if min_weight != sys.maxsize and closest:
+                self.visited[self.start_from] = True
+                self.way.insert(0, closest)
+                self.start_from = closest
+                if self.wrong_vehicle is None:
+                    del vehicles[self.start_from]
+                else:
+                    self.wrong_vehicle = None
+
+    def print_way(self):
+        return self.way
 
 
 graph_matrix = [
@@ -18,4 +60,6 @@ graph_matrix = [
 ]
 
 dijkstra = Graph(graph_matrix)
-print(dijkstra.calculate())
+dijkstra.calculate()
+
+print(dijkstra.print_way())
