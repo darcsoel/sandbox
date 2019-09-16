f2bc3d7a407bbb40aadb603c6cd46f3c6b33a0a1
Ross <rostikvunarchuk@gmail.com>
2019-09-16 23:02:06 +0300

    use copy func

--- func_params.py
+++ func_params.py
@@ -1,3 +1,6 @@
+from copy import copy
+
+
 def test_func(par):
     par += 1
     print(par)
@@ -16,3 +19,14 @@ def test_func2(list_par):
 l = [1, 2, 3]
 test_func2(l)
 print(l)
+
+
+def test_func3(list_par2):
+    list_par2 = copy(list_par2)
+    list_par2.append(1)
+    print(list_par2)
+
+
+l2 = [1, 2, 3]
+test_func3(l2)
+print(l2)
