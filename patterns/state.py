2c151e88617effc69bb48d735dc031a9c6d16613
Ross <rostikvunarchuk@gmail.com>
2019-09-28 22:10:41 +0300

    state pattern update

--- patterns/state.py
+++ patterns/state.py
@@ -1,19 +1,49 @@
 import abc
 
 
-class Context:
-    pass
-
-
 class AbstractState(metaclass=abc.ABCMeta):
-    pass
+    @abc.abstractmethod
+    def run(self):
+        pass
 
 
 class StateFirst(AbstractState):
-    pass
+    def run(self):
+        print('Start service 1')
+
+    def stop(self):
+        print('Stop service 1')
 
 
 class StateSecond(AbstractState):
-    pass
+    def run(self):
+        print('Start service 2')
+
+    def stop(self):
+        print('Stop service 2')
 
 
+class Context:
+    def __init__(self):
+        self.__state = StateFirst()
+
+    def check_first_service(self):
+        self.__state = StateFirst()
+
+    def check_second_service(self):
+        self.__state = StateSecond()
+
+    def start_service(self):
+        self.__state.run()
+
+    def stop_service(self):
+        self.__state.stop()
+
+
+context = Context()
+context.check_first_service()
+context.start_service()
+context.stop_service()
+context.check_second_service()
+context.start_service()
+context.stop_service()
