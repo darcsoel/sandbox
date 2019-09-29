8ffb32dd4eac25e7613d4c720176122e69b65d2e
Ross <rostikvunarchuk@gmail.com>
2019-09-29 22:47:52 +0300

    sockets start

---
+++ content_manager.py
@@ -0,0 +1,10 @@
+class Context:
+    def __init__(self):
+        self.x = 0
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
---
+++ socket_/socket_server.py
@@ -0,0 +1,7 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 25025
+
+if __name__ == '__main__':
+    pass
