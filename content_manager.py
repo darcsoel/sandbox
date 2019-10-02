de6318831712c2ddd5b6f9576b60422eceb0c15c
Ross <rostikvunarchuk@gmail.com>
2019-10-02 20:49:37 +0300

    simple context manager example

--- content_manager.py
+++ content_manager.py
@@ -1,10 +1,23 @@
 class Context:
     def __init__(self):
-        self.x = 0
+        self.__x = 0
 
     def __enter__(self):
-        pass
+        self.__x = 1
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+        self.__x = -1
 
+    @property
+    def x(self):
+        return self.__x
+
+
+context = Context()
+
+print(context.x)
+
+with context as c:
+    print(context.x)
+
+print(context.x)
