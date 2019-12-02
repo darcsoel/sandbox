63c52271af82d82c97834edf021c1605d4121159
Ross <rostikvunarchuk@gmail.com>
2019-12-02 22:38:19 +0200

    return logger for lru func

--- functools_/lru.py
+++ functools_/lru.py
@@ -5,14 +5,14 @@ import time
 def time_dec(func):
     def wrapper(*args, **kwargs):
         start = time.time()
-        result = func(*args, **kwargs)
+        func(*args, **kwargs)
         finish = time.time()
-        print(f'Running time - {finish - start}')
-        return result
+        return finish - start
 
     return wrapper
 
 
+@time_dec
 def fib(number: int):
     if number < 2:
         return number
@@ -20,6 +20,7 @@ def fib(number: int):
     return fib(number - 1) + fib(number - 2)
 
 
+@time_dec
 @functools.lru_cache()
 def fib_cached(number: int):
     if number < 2:
@@ -28,10 +29,13 @@ def fib_cached(number: int):
     return fib(number - 1) + fib(number - 2)
 
 
-print('Compare fibonacci functions for 10 numbers')
-print(fib(10))
-print(fib_cached(10))
+if __name__ == '__main__':
+    print('Compare fibonacci functions for 10 numbers')
+    print(fib(10))
+    print(fib_cached(10))
 
-print('\nCompare fibonacci functions for 30 numbers')
-print(fib(30))
-print(fib_cached(30))
+    print('\nCompare fibonacci functions for 30 numbers')
+    print(fib(30))
+    print(fib_cached(30))
+
+    exit()
