92f59d594538755d0cd2f6427fa76d6411f6d9bc
Ross <rostikvunarchuk@gmail.com>
2019-12-08 20:51:44 +0200

    Used method join for wait

--- gil_/multitheading_.py
+++ gil_/multitheading_.py
@@ -19,17 +19,25 @@ class Worker:
     Good realization, use class as function with method `call`
     """
 
-    def __init__(self, limit):
+    def __init__(self, name: str, limit: int):
+        self._name = name
         self._limit = limit
 
     def __call__(self, *args, **kwargs):
         for number in range(self._limit):
-            print(number)
+            print(f'{self._name} -> {number}')
 
 
 if __name__ == '__main__':
-    worker = Worker(3)
+    worker1 = Worker('first', 10)
+    worker2 = Worker('second', 10)
 
-    thread = threading.Thread(target=worker)
-    thread.run()
+    thread1 = threading.Thread(target=worker1)
+    thread2 = threading.Thread(target=worker2)
+    thread1.start()
+
+    # method join will wait for thread finish
+    # thread1.join()
+
+    thread2.start()
 
