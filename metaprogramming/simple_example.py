4e415f08f5ee3d4f0becc6f30e1d8d792ee9964a
Ross <rostikvunarchuk@gmail.com>
2020-01-01 21:49:40 +0200

    metaclass for uppercase attributes

--- metaprogramming/simple_example.py
+++ metaprogramming/simple_example.py
@@ -1,6 +1,12 @@
-class CustomMeta:
-    def __init__(self):
-        pass
+class CustomMeta(type):
+    """Make all attributes in uppercase"""
+
+    def __new__(mcs, name, bases, attrs):
+
+        attrs = ((name, value) for name, value in attrs.items() if not name.startswith('__'))
+        uppercase_attr = dict((name.upper(), value) for name, value in attrs)
+
+        return type.__new__(name, bases, uppercase_attr)
 
 
 class CustomClass(metaclass=CustomMeta):
