6937537da63e4b7e1a5672198700f500396c9a61
Ross <rostikvunarchuk@gmail.com>
2020-01-15 22:50:53 +0200

    float array to int for logistic regression fix

--- sklearn_/learning_curves.py
+++ sklearn_/learning_curves.py
@@ -4,10 +4,11 @@ from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import learning_curve
 
+_, axes = plt.subplots(2, 3, figsize=(20, 5))
 
-def render_learning_curve(train_sizes, train_scores, test_scores, fit_times):
-    _, axes = plt.subplots(1, 3, figsize=(20, 5))
-    axes[0].set_title('Learning curve')
+
+def render_learning_curve(train_sizes, train_scores, test_scores, fit_times, subplot_index):
+    axes[subplot_index, 0].set_title('Learning curve')
 
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
@@ -17,51 +18,52 @@ def render_learning_curve(train_sizes, train_scores, test_scores, fit_times):
     fit_times_std = np.std(fit_times, axis=1)
 
     # Plot learning curve
-    axes[0].grid()
-    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
-                         train_scores_mean + train_scores_std, alpha=0.1,
-                         color="r")
-    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1,
-                         color="g")
-    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
-                 label="Training score")
-    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
-                 label="Cross-validation score")
-    axes[0].legend(loc="best")
+    axes[subplot_index, 0].grid()
+    axes[subplot_index, 0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                                        train_scores_mean + train_scores_std, alpha=0.1,
+                                        color="r")
+    axes[subplot_index, 0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                                        test_scores_mean + test_scores_std, alpha=0.1,
+                                        color="g")
+    axes[subplot_index, 0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                                label="Training score")
+    axes[subplot_index, 0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                                label="Cross-validation score")
+    axes[subplot_index, 0].legend(loc="best")
 
     # Plot n_samples vs fit_times
-    axes[1].grid()
-    axes[1].plot(train_sizes, fit_times_mean, 'o-')
-    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
-                         fit_times_mean + fit_times_std, alpha=0.1)
-    axes[1].set_xlabel("Training examples")
-    axes[1].set_ylabel("fit_times")
-    axes[1].set_title("Scalability of the model")
+    axes[subplot_index, 1].grid()
+    axes[subplot_index, 1].plot(train_sizes, fit_times_mean, 'o-')
+    axes[subplot_index, 1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                                        fit_times_mean + fit_times_std, alpha=0.1)
+    axes[subplot_index, 1].set_xlabel("Training examples")
+    axes[subplot_index, 1].set_ylabel("fit_times")
+    axes[subplot_index, 1].set_title("Scalability of the model")
 
     # Plot fit_time vs score
-    axes[2].grid()
-    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
-    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1)
-    axes[2].set_xlabel("fit_times")
-    axes[2].set_ylabel("Score")
-    axes[2].set_title("Performance of the model")
-
-    plt.show()
+    axes[subplot_index, 2].grid()
+    axes[subplot_index, 2].plot(fit_times_mean, test_scores_mean, 'o-')
+    axes[subplot_index, 2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                                        test_scores_mean + test_scores_std, alpha=0.1)
+    axes[subplot_index, 2].set_xlabel("fit_times")
+    axes[subplot_index, 2].set_ylabel("Score")
+    axes[subplot_index, 2].set_title("Performance of the model")
 
 
 if __name__ == '__main__':
+    # x, y = make_regression(random_state=42)
+    # train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(LinearRegression(), x, y, cv=7,
+    #                                                                       n_jobs=5, return_times=True)
+    # render_learning_curve(train_sizes, train_scores, test_scores, fit_times, 0)
+
     x, y = make_regression(random_state=42)
+    x = x * 10
+    x = x.astype(int)
+    y = y * 10
+    y = y.astype(int)
+    train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(LogisticRegression(), x, y, cv=2,
+                                                                          n_jobs=2, return_times=True, verbose=3)
+    render_learning_curve(train_sizes, train_scores, test_scores, fit_times, 1)
 
-    train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(LinearRegression(), x, y, cv=7,
-                                                                          n_jobs=5, return_times=True)
-
-    render_learning_curve(train_sizes, train_scores, test_scores, fit_times)
-
-    train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(LogisticRegression(), x, y, cv=7,
-                                                                          n_jobs=5, return_times=True)
-
-    render_learning_curve(train_sizes, train_scores, test_scores, fit_times)
-
+    plt.show()
     exit()
