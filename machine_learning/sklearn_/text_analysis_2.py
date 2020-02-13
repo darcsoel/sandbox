76e1264331847e0ea69c41450d972d5df4128737
Ross <rostikvunarchuk@gmail.com>
2020-02-13 22:58:39 +0200

    pipeline text analysis

---
+++ machine_learning/sklearn_/text_analysis_2.py
@@ -0,0 +1,29 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set()
+
+model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+
+categories = ['alt.atheism', 'talk.religion.misc', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+
+train = fetch_20newsgroups(categories=categories)
+test = fetch_20newsgroups(categories=categories, subset='test')
+
+model.fit(train.data, train.target)
+labels = model.predict(test.data)
+
+matrix = confusion_matrix(test.target, labels)
+sns.heatmap(matrix.T, square=True, annot=True, xticklabels=train.target_names, yticklabels=train.target_names)
+
+plt.xlabel('true')
+plt.ylabel('predicted')
+
+plt.show()
+
+exit()
