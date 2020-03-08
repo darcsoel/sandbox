aa430d3e7e1bde0149f8ed4ac96809619ad5a208
Ross <rostikvunarchuk@gmail.com>
2020-03-08 22:14:01 +0200

    pca variance ratio

--- machine_learning/sklearn_/basics&examples/dimensionality_reduction/digits_pca.py
+++ machine_learning/sklearn_/basics&examples/dimensionality_reduction/digits_pca.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
 
@@ -8,9 +9,18 @@ model.fit(digits)
 
 transformed = model.transform(digits)
 
+plt.subplot(2, 2, 1)
+
 plt.scatter(transformed[:, 0], transformed[:, 1], c=label, alpha=0.5, cmap='rainbow')
 plt.xlabel('component1')
 plt.ylabel('component2')
 plt.colorbar()
+
+check_model = PCA()
+check_model.fit(digits)
+plt.subplot(2, 2, 2)
+plt.plot(np.cumsum(check_model.explained_variance_ratio_))
+plt.xlabel('n components')
+plt.ylabel('accuracy')
 
 plt.show()
