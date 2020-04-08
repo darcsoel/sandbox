588b6819a68c12417aa184740db6c6b849c0b5f4
Ross <rostikvunarchuk@gmail.com>
2020-04-08 22:45:43 +0300

    simple lab task; start face finder algorithm

---
+++ labs/swap_numbers.py
@@ -0,0 +1,17 @@
+from sys import exit
+
+
+def main():
+    numbers = input('Enter whitespace-separate numbers')
+    first, second = numbers.split(' ')
+
+    if not first and not second:
+        raise SystemError('You entered just one number')
+
+    print(f'Reversed => {second}; {first}')
+    return
+
+
+if __name__ == '__main__':
+    main()
+    exit()
---
+++ machine_learning/sklearn_/tasks/face_finder.py
@@ -0,0 +1,12 @@
+from sys import exit
+
+from skimage import color, data
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    exit()
