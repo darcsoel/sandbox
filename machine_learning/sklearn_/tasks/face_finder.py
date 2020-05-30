f0491396b37a1743a7f3d6f50854e17d51ad3485
Ross <rostikvunarchuk@gmail.com>
2020-05-30 22:27:43 +0300

    extract hog from example image

--- machine_learning/sklearn_/tasks/face_finder.py
+++ machine_learning/sklearn_/tasks/face_finder.py
@@ -1,6 +1,23 @@
 from sys import exit
 
-from skimage import color, data
+import matplotlib.pyplot as plt
+from skimage import color, data, feature
+
+
+def extract_hog():
+    chelsea = data.chelsea()
+    image = color.rgb2gray(chelsea)
+    hog_vec, hog_vis = feature.hog(image, visualize=True)
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6), subplot_kw={'xticks': [], 'yticks': []})
+
+    ax[0].imshow(image)
+    ax[0].set_title('original')
+
+    ax[1].imshow(hog_vis)
+    ax[1].set_title('hog')
+
+    plt.show()
 
 
 def main():
@@ -8,5 +25,6 @@ def main():
 
 
 if __name__ == '__main__':
+    extract_hog()
     main()
     exit()
