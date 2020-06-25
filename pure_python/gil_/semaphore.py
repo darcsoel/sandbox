37d26b186e9ebc7fba12cab980a593a8bfb7f6bf
Ross <rostikvunarchuk@gmail.com>
2020-06-25 22:42:39 +0300

    add directory for some files; add event to threads.

--- pure_python/gil_/semaphore.py
+++ pure_python/gil_/semaphore.py
@@ -3,8 +3,8 @@ import threading
 from sys import exit
 
 max_workers = 2
-min_number = 7
-max_number = 15
+min_number = 10
+max_number = 20
 
 
 class Worker:
@@ -12,25 +12,31 @@ class Worker:
     Class demonstrate semaphore functional. While
     """
 
-    def __init__(self, worker_name, number, semaphore=None):
+    def __init__(self, worker_name, number, semaphore=None, event=None):
         self._worker_name = worker_name
         self._number = number
         self._semaphore = None
+        self._event = None
 
         if isinstance(semaphore, threading.BoundedSemaphore):
             self._semaphore = semaphore
+        if isinstance(event, threading.Event):
+            self._event = event
 
     def __call__(self, *args, **kwargs):
-        self._semaphore.acquire()
+        if self._semaphore:
+            self._semaphore.acquire()
 
         for i in range(self._number):
             print(f'{self._worker_name} -> {i}\n')
 
-        self._semaphore.release()
+        if self._semaphore:
+            self._semaphore.release()
 
 
 def main():
     semaphore = threading.BoundedSemaphore(max_workers)
+    event = threading.Event()
     rand_number = random.randint(min_number, max_number)
 
     print(f'Max number = {rand_number}')
@@ -41,7 +47,7 @@ def main():
 
     thread1.start()
     thread2.start()
-    thread3.start()
+    thread3.start()  # sometimes it do not called
 
 
 if __name__ == '__main__':
