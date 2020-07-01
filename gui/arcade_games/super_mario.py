a4ffdb8bbe776afec7d150f6e6c4937057c71dc4
Ross <rostikvunarchuk@gmail.com>
2020-07-01 13:55:28 +0300

    Start update Mario game

--- gui/arcade_games/super_mario.py
+++ gui/arcade_games/super_mario.py
@@ -2,26 +2,19 @@ import arcade
 import time
 from random import randint
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 JUMP_HEIGHT = 140
 MOVE_SPEED = 100
-MIN_HEIGHT = 100
+MIN_HEIGHT = 150
+MIN_WIDTH = 150
+GROUND_BLOCKS_DISTANCE = 100
+GROUND_BLOCKS_COUNT = 11
+COINS_COUNT = 6
+COINS_DISTANCE = 140
 
 
 class MarioWindow(arcade.Window):
-    def _create_coin(self, index):
-        coin = arcade.Sprite('images/gold.png', 0.1)
-        coin.center_y = MIN_HEIGHT + randint(110, 220)
-        coin.center_x = index * 140
-        self.coin_list.append(coin)
-
-    def _create_ground(self, index):
-        ground = arcade.Sprite('images/ground.png', 0.5)
-        ground.center_y = 90
-        ground.center_x = index * MIN_HEIGHT
-        self.ground_list.append(ground)
-
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, title='Super Mario Game')
         arcade.set_background_color(arcade.color.ANDROID_GREEN)
@@ -32,16 +25,28 @@ class MarioWindow(arcade.Window):
         self.ground_list = arcade.SpriteList()
 
         self.player = arcade.Sprite("images/mario_run.png", 0.1)
-        self.player.center_y = 150
-        self.player.center_x = MIN_HEIGHT
+        self.player.center_y = MIN_HEIGHT
+        self.player.center_x = MIN_WIDTH
 
-        for i in range(0, 9):
+        for i in range(0, GROUND_BLOCKS_COUNT):
             self._create_ground(i)
 
-        for i in range(1, 6):
+        for i in range(1, COINS_COUNT):
             self._create_coin(i)
 
         self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.coin_list)
+
+    def _create_coin(self, index):
+        coin = arcade.Sprite('images/gold.png', 0.1)
+        coin.center_y = MIN_HEIGHT + randint(110, 220)
+        coin.center_x = index * COINS_DISTANCE
+        self.coin_list.append(coin)
+
+    def _create_ground(self, index):
+        ground = arcade.Sprite('images/ground.png', 0.5)
+        ground.center_y = 90
+        ground.center_x = index * GROUND_BLOCKS_DISTANCE
+        self.ground_list.append(ground)
 
     def on_draw(self):
         """Render the screen. """
@@ -52,22 +57,23 @@ class MarioWindow(arcade.Window):
         self.coin_list.draw()
         self.ground_list.draw()
 
-        # Put the text on the screen.
-        output = "Score: {}".format(self.score)
+        output = f"Score: {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
 
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.W or symbol == arcade.key.SPACE:
             self.player.center_y += JUMP_HEIGHT
-        if symbol == arcade.key.A and self.player.center_x > 0:
+        elif symbol == arcade.key.A and self.player.center_x > MIN_WIDTH:
             self.player.center_x -= MOVE_SPEED
-        if symbol == arcade.key.D and self.player.center_x < SCREEN_WIDTH:
+        elif symbol == arcade.key.D and self.player.center_x + MIN_WIDTH < SCREEN_WIDTH:
             self.player.center_x += MOVE_SPEED
+        else:
+            return
 
     def on_key_release(self, symbol: int, modifiers: int):
         if symbol == arcade.key.W or symbol == arcade.key.SPACE:
             time.sleep(0.1)
-            self.player.center_y -= JUMP_HEIGHT
+            self.player.center_y = MIN_WIDTH
 
     def update(self, delta_time: float):
         self.physics_engine.update()
