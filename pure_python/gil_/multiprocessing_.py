cd16d3a9ddea175558551f75419d0ef1465bf8a2
Ross <rostikvunarchuk@gmail.com>
2020-07-03 22:53:39 +0300

    try multiprocessing

--- pure_python/gil_/multiprocessing_.py
+++ pure_python/gil_/multiprocessing_.py
@@ -1,10 +1,36 @@
 import multiprocessing
+import os
+import random
+from sys import exit
 
 
 class Process:
-    def __init__(self):
-        pass
+    def __init__(self, number):
+        self._number = number
+
+    def __call__(self, *args, **kwargs):
+        proc = os.getpid()
+        n = self._number
+
+        print(f"ID {proc} -> number {n}")
 
 
 def main():
-    pass
+    random_list = random.sample(range(10, 30), 5)
+    processes = []
+
+    for number in random_list:
+        process = multiprocessing.Process(target=Process(number))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    for process in processes:
+        process.kill()
+
+
+if __name__ == '__main__':
+    main()
+    exit()
