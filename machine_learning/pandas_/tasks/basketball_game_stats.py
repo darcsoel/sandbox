9f3a96cab2b438bf328dd191d00820b050fb0a5b
Ross <rostikvunarchuk@gmail.com>
2020-08-25 21:53:06 +0300

    add basketball csv file

---
+++ machine_learning/pandas_/tasks/basketball_game_stats.py
@@ -0,0 +1,24 @@
+import sys
+import pandas as pd
+
+
+def find_most_successfull_school(dataset):
+    pass
+
+
+def find_players_with_max_played_games(dataset):
+    pass
+
+
+def find_most_successfull_players(dataset):
+    pass
+
+
+def main():
+    players_data_file = pd.read_csv('../../../players_stats_by_season_full_details.csv')
+    return
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit()
